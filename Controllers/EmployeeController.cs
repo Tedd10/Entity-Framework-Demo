810746e365fa17d3776488987b19c9f802d6b548
Theodore <teddyakpati@gmail.com>
2024-02-29 11:21:02 -0600

    Add project files.

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,25 @@
+﻿using EFDEMO.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EFDEMO.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult Index()
+        {
+            EFDEMOEntities entities = new EFDEMOEntities();
+
+            // Get the list of employees from the database
+            List<Employee> employees = entities.Employees.ToList();
+
+            // Pass the list of employees to the view
+            return View(employees);
+
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="EFDEMO.MvcApplication" Language="C#" %>
